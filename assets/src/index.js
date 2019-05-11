3ba636e488664d9204dbe69c77c67c73e48819d8
Dave Kanter <moatgoat@hotmail.com>
2019-05-10 19:53:20 -0700

    Files re-configured for the start of Part 2

--- assets/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-const App = () => {
-  return (
-    <div>DAVE RULES!</div>
-  );
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
